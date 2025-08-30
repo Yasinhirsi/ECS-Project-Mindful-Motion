ea12e05415544902da1ade3014251f4d4018322f
Yasinhirsi <yasinhirsi3@gmail.com>
2025-08-30 10:18:29 +0100

    fixed comment which caused syntax error

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket       = "mindful-motion-terraform-state-487148038595"
     key          = "mindful-motion/terraform.tfstate"
-    region       = "eu-west-2" //var.region wouldnt work as backend is processed first
+    region       = "eu-west-2" ##var.region wouldnt work as backend is processed first
     encrypt      = true
-    use_lockfile = true // for state locking (s3 state locking as dynamodb is depracated)
+    use_lockfile = true ## for state locking (s3 state locking as dynamodb is depracated)
   }
 }
